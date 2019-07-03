755dcf2a14ce2b2759c75beb8671f174938deaf9
hamad789 <Ahmedturki810@gmail.com>
2019-07-03 14:45:51 +0200

    Added comments in code regarding starting new analysis

--- bikeshare.py
+++ bikeshare.py
@@ -225,11 +225,11 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
+        time_stt_stats(df)
         trip_duration_stats(df)
         user_stats(df)
 
+        #Ask user if he wants to start over the analysis
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
